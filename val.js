b6d5abf566f07b9d447ef597f3c608b4a220bb36
tomergahtan <121878273+tomergahtan@users.noreply.github.com>
2024-02-09 08:11:33 +0200

    Update val.js

--- val.js
+++ val.js
@@ -1,18 +1,20 @@
-document.getElementById('imageToClick').addEventListener('click', function(e) {
+document.getElementById('imageToClick').addEventListener('click', function (e) {
     'use strict';
     const rect = e.target.getBoundingClientRect();
     const imageCenterX = rect.left + (rect.width / 2);
     let imageCenterY = rect.top + (rect.height / 2); 
 
     // Adjust for viewport width for specific devices
-    if (window.innerWidth <= 760) {
-        imageCenterY += 20; // Adjust based on needs for Galaxy S10 size
-    }
+
 
     const numberOfHearts = 5; 
 
-    const radius = Math.max(rect.width, rect.height) / 2 + 30; 
-
+    let radius = Math.max(rect.width, rect.height) / 2 + 30; 
+    if (window.innerWidth <= 760) {
+        imageCenterY -= 0;
+        radius-=15;// Adjust based on needs for Galaxy S10 size
+    }
+    
     for (let i = 0; i < numberOfHearts; i++) {
         const heart = document.createElement('div');
         heart.classList.add('heart');
