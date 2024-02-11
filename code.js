99886de22af9e819c1ae331e786d3fc1499c0134
tomergahtan <121878273+tomergahtan@users.noreply.github.com>
2024-02-11 13:37:43 +0200

    update

---
+++ code.js
@@ -0,0 +1,15 @@
+        
+
+
+
+
+
+
+
+
+
+function displayScreenSize() {
+    'use strict';
+            var screenSizeDiv = document.getElementById('screen-size');
+            screenSizeDiv.innerHTML = "Screen Width: " + window.screen.width + "px, Screen Height: " + window.screen.height + "px";
+        }
