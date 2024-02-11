ed240b0a3afc7a2bcc6d3b695f562dc454908a62
tomergahtan <121878273+tomergahtan@users.noreply.github.com>
2024-02-11 13:57:33 +0200

    Update code.js

--- code.js
+++ code.js
@@ -14,12 +14,3 @@ function displayScreenSize() {
             screenSizeDiv.innerHTML = "Screen Width: " + window.screen.width + "px, Screen Height: " + window.screen.height + "px";
         }
 
-function displayScreenSize() {
-    const pixelsPerInch = 96;
-    const cmPerInch = 2.54;
-    const widthInCm = (window.screen.width / pixelsPerInch) * cmPerInch;
-    const heightInCm = (window.screen.height / pixelsPerInch) * cmPerInch;
-    
-    const screenSizeDiv = document.getElementById('screen-size');
-    screenSizeDiv.innerHTML = "Screen Width: " + widthInCm.toFixed(2) + " cm, Screen Height: " + heightInCm.toFixed(2) + " cm";
-}
